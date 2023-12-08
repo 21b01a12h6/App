dbc640582dae8ca00ed40d96ec4cc4361076ac07
NithyaSri24 <21b01a12h6@svecw.edu.in>
2023-12-08 10:41:11 +0530

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,58 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routers/auth.js";
+import usersRoute from "./routers/users.js";
+import hotelsRoute from "./routers/hotels.js";
+import roomsRoute from "./routers/rooms.js";
+import cookieParser from "cookie-parser";
+import cors from "cors"
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("connected to mongoDB server")
+    } catch (error) {
+        throw error;
+    }
+};
+
+mongoose.connection.on("connected", () => {
+    console.log("mongoDB connected!")
+})
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB disconnected!")
+})
+
+// app.get("/", (req,res) => {
+//     res.send("This is my first request..")
+// })
+//middlewares
+
+app.use(cors())
+app.use(cookieParser())
+app.use(express.json())
+app.use("/api/auth", authRoute); 
+app.use("/api/hotels", hotelsRoute); 
+app.use("/api/rooms", roomsRoute); 
+app.use("/api/users", usersRoute); 
+
+app.use((err, req, res,next)=>{
+    const errorStatus = err.status || 500
+    const errorMessage = err.message || "Something went wrong"
+    return res.status(errorStatus).json({
+        success : false,
+        status : errorStatus,
+        message : errorMessage,
+        stack : err.stack
+    })
+})
+
+app.listen(8800, () => {
+    connect()
+    console.log("connected to backend..!");
+});
